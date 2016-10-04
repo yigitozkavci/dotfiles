d7d119ebd86aee444a8655b946d93c7d96dcfe0f
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2016-10-04 10:55:10 +0300

    Add vim colors and update installation script.

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 dir=~/dotfiles
 files=$(find *)
+vim_path="~/.vim"
+vim_colors_path="~/.vim/colors"
+vim_colors=$(find .vim/colors/*)
 for file in $files; do
-    ln -s $dir/$file ~/.$file
+  ln -s $dir/$file ~/.$file
+done
+
+for color in $vim_colors; do
+  ln -s $dir/$color ~/$color
 done
