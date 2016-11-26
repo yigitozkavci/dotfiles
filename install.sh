96f6840b337340cd2fa371f2153adca9c5dd974a
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2016-11-26 09:08:33 +0300

    Rebuild directory structure for the script

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 dir=~/dotfiles
-files=$(find *)
+dotfile_dir=~/dotfiles/dotfiles
+files=$(find ./dotfiles/* -type f -exec basename {} \;)
 vim_path="~/.vim"
 vim_colors_path="~/.vim/colors"
 vim_colors=$(find .vim/colors/*)
 for file in $files; do
-  ln -s $dir/$file ~/.$file
+  if [ -f $dotfile_dir/$file ]; then
+    echo "Checking: ~/.$file "
+    ln -s $dir/$file ~/.$file
+    echo "File connected: $dir/$file"
+  fi
 done
 
 for color in $vim_colors; do
-  ln -s $dir/$color ~/$color
+  if ! [ -f $dir/$color ]; then
+    ln -s $dir/$color ~/$color
+  fi
 done
+
+echo "Install completed."
